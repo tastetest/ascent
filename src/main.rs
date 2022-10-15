cad4d04e7af0851806a856ef5f4fc50ca0c6699f
tastetest <tastytothesupreme@gmail.com>
2022-10-15 13:30:32 -0600

    initialized files

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use bevy::prelude::*;
+use bevy::render::texture::*;
+use rapier2d::prelude::*;
+
+mod player;
+mod texture_atlas;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ImageSettings::default_nearest())
+        .add_system(bevy::window::close_on_esc)
+        .add_system(texture_atlas::setup)
+        .add_startup_system(setup_camera)
+        .run()
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn_bundle(Camera2dBundle::default());
+}
---
+++ src/texture_atlas.rs
@@ -0,0 +1,17 @@
+use bevy::prelude::*;
+
+pub fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load("ascii.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(20.0, 20.0), 2, 2);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    commands.spawn_bundle(SpriteSheetBundle {
+        texture_atlas: texture_atlas_handle,
+        transform: Transform::from_scale(Vec3::splat(6.0)),
+        ..default()
+    });
+}
